0d18f2c84043777ea6ebe8c6f698054c3cf8cf60
Gianmarco <gianmarco.cavaliere19093@gmail.com>
2021-01-19 17:27:34 +0100

    Try and Catch Added

--- employe_commission.php
+++ employe_commission.php
@@ -22,6 +22,13 @@ class employe_commission extends employes
     {
         parent:: __construct($name, $last_name, $cf, $employer_code, $fee);
 
+        if (!is_string($project_name)) {
+            throw new Exception("Non e' una stringa ");
+        }
+        if (!is_int($project_fee)) {
+            throw new Exception("Non e' un numero ");
+        }
+
         $this->project_name = $project_name;
         $this->project_fee = $project_fee;
     }
--- index.php
+++ index.php
@@ -8,7 +8,12 @@ require 'hour_employes.php';
 
 $impiegato_salariato = new employes_fullTime('Gianmarco', 'Cavaliere', 'Mario', 1020, 5, 30, 30);
 
-$impiegato_commissione = new employe_commission('Baluardo', 'Giovani', 'piropiro', '136', 'irbudello', 'MySite', 800, );
+try {
+    $impiegato_commissione = new employe_commission('Baluardo', 'Giovani', 'piropiro', '136', 'irbudello', 'MySite', 'pippo', );
+} catch (Exception $e) {
+    $impiegato_commissione = new employes('Pippo', 'Pluto', 'Paperino', 'CFCF', 200, 5);
+    echo $e->getMessage();
+}
 
 $impiegato_commissione->toString();
 $impiegato_commissione->profitCalculation();
